6046fa4d045816f37f41696f465614c9dc59e5a9
250541017-web <250541017@firat.edu.tr>
2025-10-23 18:01:23 +0300

    AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,10 @@
+public class AsciiMelek {
+    public static void main(String[] args) {
+
+        System.out.println("M     M  EEEEE  L      EEEEE  K   /");
+        System.out.println("MM   MM  E      L      E      K  / ");
+        System.out.println("M M M M  EEEE   L      EEEE   K /  ");
+        System.out.println("M  M  M  E      L      E      K  / ");
+        System.out.println("M     M  EEEEE  LLLLL  EEEEE  K   /");
+    }
+}
